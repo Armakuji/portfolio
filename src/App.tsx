5bb1a1740a081f393384199588af0459a814dc1f
Armakuji <armakuji@hotmail.com>
2021-10-27 23:32:10 +0700

    fix(Deploy): try to import antd css in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "antd/dist/antd.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "utils/theme";
 import LandingPage from "pages/LandingPage/LandingPage";
